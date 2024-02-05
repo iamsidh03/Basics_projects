ecd4fbb4a6a50ba36ce49d8362e889b6b2adfe22
SIDDHARTH RAJ <118841155+iamsidh03@users.noreply.github.com>
2024-02-05 17:06:29 +0000

    data type

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,11 @@
+const accountId =87878
+let accountEmail ="raj@gmail.com"
+var accountPassword="24343"
+accountCity="Jaipur"
+let accountState;
+/*
+const can't change
+try not to use var because of issue in block scope and functional scopes
+
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
