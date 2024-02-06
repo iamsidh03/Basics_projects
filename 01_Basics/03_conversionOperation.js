ee2d144e55efed80b82fb7589e978d017d2c7fa3
SIDDHARTH RAJ <118841155+iamsidh03@users.noreply.github.com>
2024-02-06 15:01:05 +0000

    Convertion Of DataTyoe

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+//let score=undefined
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+
+
+//valueInNumber
+//"33" =>33
+// "33abc" =>NaN
+// true =>1; false =>0
+// undefined => number
+
+// let isLoggedIn=0
+// let booleanLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanLoggedIn)
+
+//1 =>true ; 0 =>false
+//   "" => false
+// "notEmpty" =>true
+
+let someNumber=33
+let stringNumber =String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
