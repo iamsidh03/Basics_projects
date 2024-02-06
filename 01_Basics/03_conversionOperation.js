388ce4a79ff9afd66bd47b96c8719a91fbc637a2
SIDDHARTH RAJ <118841155+iamsidh03@users.noreply.github.com>
2024-02-06 17:37:18 +0000

    avoidTheseComparition

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -19,7 +19,67 @@
 //   "" => false
 // "notEmpty" =>true
 
-let someNumber=33
-let stringNumber =String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber=33
+// let stringNumber =String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//***********************OPERATIONS********************** */
+
+/* how to print neg of any value */
+// let value =3
+// let negValue =-value
+// console.log(negValue)
+
+/* */
+//  let str1 ="Hello"
+//  let str2 =" Siddharth"
+//  let str3 =str1 +str2
+//  console.log(str3)
+ 
+/*
+ console.log("1" +2); //  O/P   =>  12
+ console.log("1"+2+3)  // O/P   =>  123
+ console.log(1+23+"65") //O/P   =>  2465
+ console.log("234"+(2+5));  //  O/P 2347
+ */
+
+ /*  */
+// console.log(+true) //   O/P =>  1
+// console.log(+"");  //   O/p =>  0
+
+// let num1,num2,num3;
+// num1=num2=num3=2+2
+// console.log(num1)
+ 
+
+/* prefix and postfix operator */
+/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment */
+// let gameCounter =100
+// gameCounter++;
+// console.log(gameCounter)
+
+/********************comparition*********************** */
+
+//consol.log(2!=1);
+
+// console.log("2">1)
+// console.log("02">1)
+
+//console.log(null>0)
+//console.log(null ==0)  //false
+//console.log(null >=0)  //true
+//console.log(null<0)
+
+//console.log(undefined==0)  //false
+//console.log(undefined<=0)  //false
+//console.log(undefined>=0)   //false
+
+// ===
+//console.log("2" === 2); //false
+
+/******AVOID THESE TYPE OF COMPARITION*******/
+
+
+
